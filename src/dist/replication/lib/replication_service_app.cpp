be287828c2e54320add3ccabc1830ec5918d52de
Shuo-Jia <js982986555@live.com>
2020-07-14 01:20:03 +0800

    format

--- src/dist/replication/lib/replication_service_app.cpp
+++ src/dist/replication/lib/replication_service_app.cpp
@@ -87,6 +87,8 @@ void replication_service_app::on_intercepted_request(dsn::gpid gpid,
                                                      bool is_write,
                                                      dsn::message_ex *msg)
 {
+    msg->request_latency_tracer = std::make_shared<dsn::tool::latency_tracer>(
+        msg->header->id, "on_intercepted_request", "request");
     if (is_write) {
         _stub->on_client_write(gpid, msg);
     } else {
