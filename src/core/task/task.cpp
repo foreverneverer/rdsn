12b5934554adcd42cb0e6dd907edc3c6862f506a
Shuo-Jia <js982986555@live.com>
2020-07-15 01:02:57 +0800

    update

--- src/core/task/task.cpp
+++ src/core/task/task.cpp
@@ -148,7 +148,7 @@ task::~task()
         _wait_event.store(nullptr);
     }
 
-    ltracer->dump_trace_points(100);
+    ltracer->dump_trace_points(20000000);
 }
 
 bool task::set_retry(bool enqueue_immediately /*= true*/)
