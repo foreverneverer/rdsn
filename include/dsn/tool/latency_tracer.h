33175f8bbae22d9fe3eba47a4b4c3f2139a37f90
JiaShuo <js982986555@live.com>
2020-07-16 15:00:52 +0800

    add task id to see complete id

--- include/dsn/tool/latency_tracer.h
+++ include/dsn/tool/latency_tracer.h
@@ -77,7 +77,7 @@ public:
 
         int64_t ts = dsn_now_ns();
         dsn::zauto_write_lock write(lock);
-        points[ts] = fmt::format("{}|{}", name, dsn::task::get_current_task_id());
+        points[ts] = fmt::format("{}|{}", name, ::dsn::utils::get_current_tid());
     }
 
     void open_trace(bool open) { is_open = open; }
